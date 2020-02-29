8842ea2eac789495131b3ac6e8c0779f7aab273d
Álvaro <alvaritovg93@hotmail.com>
2020-02-29 14:25:22 +0100

    Installed libraries: redux and styled-components

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+// {
+//   root: '<rootDir>/tests'
+//   roots: ['<rootDir>/tests/'],
+//   testMatch: [
+//     '**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'
+//   ]
+// }
+
+// module.exports = {
+//   roots: ['<rootDir>/src', '<rootDir>/tests'],
+//   testMatch: [
+//     '<rootDir>/tests/**/*.js?(x)',
+//     '<rootDir>/tests/?(*.)+(spec|test).js?(x)',
+//   ],
+// }
--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
-function App() {
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+const App = () => {
   return (
-    <div>
-      <h1>TODO APP</h1>
-    </div>
+    <Wrapper>
+      <Title>Hello World!</Title>
+    </Wrapper>
   )
 }
 
