3ef638cab2c57dd00a676e926865f829e549d6ee
Álvaro <alvaritovg93@hotmail.com>
2020-05-12 17:58:02 +0200

    Add list items to aside

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import Header from './ui/header/Header'
 import { ReactComponent as StarIcon } from './assets/icons/star.svg'
+import { ReactComponent as TasksIcon } from './assets/icons/tasks.svg'
 
 const MainWrapper = styled.section`
   display: grid;
@@ -16,7 +17,8 @@ const MainWrapper = styled.section`
 
 const Aside = styled.aside`
   grid-area: aside;
-  border-right: 1px solid #c3c2c2;
+  background: #d0cdcd7d;
+  border-right: 1px solid #e5e5e5;
 `
 
 const Ul = styled.ul`
@@ -24,7 +26,7 @@ const Ul = styled.ul`
   list-style: none;
 `
 
-const ImportantItem = styled.div`
+const ListItem = styled.div`
   display: flex;
   align-items: center;
 `
@@ -34,9 +36,42 @@ const StarIconStyled = styled(StarIcon)`
   margin-right: 7px;
 `
 
+const TasksIconStyled = styled(TasksIcon)`
+  width: 12px;
+  margin-right: 7px;
+  margin-left: 2px;
+`
+
+const Counter = styled.p`
+  margin-left: auto;
+`
+
 const Content = styled.section`
   grid-area: content;
   margin: 0 24px;
+  display: flex;
+  flex-direction: column;
+  // background: linear-gradient(
+  //   180deg,
+  //   white,
+  //   white 52px,
+  //   #e5e5e5 52px,
+  //   #e5e5e5 52px
+  // );
+  // background-size: 100% 53px;
+  // box-shadow: inset 0 1px 0 0 #e5e5e5;
+`
+
+const HeaderContent = styled.div`
+  font-size: 20px;
+  color: #3e69e4;
+  margin: 15px 0px;
+`
+
+const TasksList = styled.div`
+  // grid-area: content;
+  // margin: 0 24px;
+  height: 100%;
   background: linear-gradient(
     180deg,
     white,
@@ -46,6 +81,14 @@ const Content = styled.section`
   );
   background-size: 100% 53px;
   box-shadow: inset 0 1px 0 0 #e5e5e5;
+  // height: 100%;
+  // overflow: hidden;
+`
+
+const TaskItem = styled.div`
+  display: flex;
+  align-items: center;
+  min-height: 52px;
 `
 
 const App = () => {
@@ -55,15 +98,35 @@ const App = () => {
       <Aside>
         <Ul>
           <li>
-            <ImportantItem>
+            <ListItem>
               <StarIconStyled />
               <p>Importante</p>
-            </ImportantItem>
+              <Counter>1</Counter>
+            </ListItem>
           </li>
-          <li>Tareas</li>
+          <li>
+            <ListItem>
+              <TasksIconStyled />
+              <p>Tareas</p>
+              <Counter>3</Counter>
+            </ListItem>
+          </li>
         </Ul>
       </Aside>
-      <Content />
+      <Content>
+        <HeaderContent>
+          <p>Tareas</p>
+        </HeaderContent>
+        {/* <AddContent>
+          <p>Agregar una tarea</p>
+        </AddContent> */}
+        <TasksList>
+          <TaskItem>
+            <p>X</p> {/* Checkbox icon */}
+            <p>Comprar</p>
+          </TaskItem>
+        </TasksList>
+      </Content>
     </MainWrapper>
   )
 }
