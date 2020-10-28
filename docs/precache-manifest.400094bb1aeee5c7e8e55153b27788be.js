0297ed7a637011e4126f055979c3ade95a37ac37
Álvaro <alvaritovg93@hotmail.com>
2020-10-28 10:21:13 +0100

    :rocket: New build

---
+++ docs/precache-manifest.400094bb1aeee5c7e8e55153b27788be.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b02737f077ee6f2b8045e97354572f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea534fc2e8d4a32e6427",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "da2801b32f9299056644",
+    "url": "./static/css/main.4fcc6d38.chunk.css"
+  },
+  {
+    "revision": "ea534fc2e8d4a32e6427",
+    "url": "./static/js/2.8a0750c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.8a0750c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da2801b32f9299056644",
+    "url": "./static/js/main.003bca3f.chunk.js"
+  },
+  {
+    "revision": "893e2b16288e61bfaa72",
+    "url": "./static/js/runtime-main.182c9327.js"
+  },
+  {
+    "revision": "759984802129a2dc5eda4682155d9d01",
+    "url": "./static/media/checkbox.75998480.svg"
+  },
+  {
+    "revision": "7a0423bd6098151cfe14f7d230cff8c5",
+    "url": "./static/media/checkbox_checked.7a0423bd.svg"
+  },
+  {
+    "revision": "c4c396d3afb908f878b4835a616ba1af",
+    "url": "./static/media/close.c4c396d3.svg"
+  },
+  {
+    "revision": "85cdde2801807c2e8bc1fc90c7fb7ed8",
+    "url": "./static/media/completed.85cdde28.svg"
+  },
+  {
+    "revision": "29e44c512052159c07eb6a18341b3e71",
+    "url": "./static/media/list.29e44c51.svg"
+  },
+  {
+    "revision": "a450cc103b020589bf97577c53c009d7",
+    "url": "./static/media/menu.a450cc10.svg"
+  },
+  {
+    "revision": "2ea4c43614e5b0beff1d450e9d280ea2",
+    "url": "./static/media/searcher.2ea4c436.svg"
+  },
+  {
+    "revision": "d1d08460bfd28080eb4d570f6baeaa60",
+    "url": "./static/media/star.d1d08460.svg"
+  },
+  {
+    "revision": "d1d08460bfd28080eb4d570f6baeaa60",
+    "url": "./static/media/star_big.d1d08460.svg"
+  },
+  {
+    "revision": "37857ad3f846f65476cffb20d3dfc4e5",
+    "url": "./static/media/star_big_filled.37857ad3.svg"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "./static/media/trash.650cdc57.svg"
+  },
+  {
+    "revision": "b19a55a2c5794d99aa3481f287f9f3d1",
+    "url": "./static/media/user.b19a55a2.svg"
+  }
+]);
